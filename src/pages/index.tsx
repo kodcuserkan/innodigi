84efc01e3d0b32dd9d8402c2ff9a3c6c08833158
Serkan Akman <akmanserkan65@gmail.com>
2022-09-02 14:10:49 +0300

    subject: homepage small change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
+import Image from 'next/image';
 import * as React from 'react';
 
 import Layout from '@/components/layout/Layout';
 import UnderlineLink from '@/components/links/UnderlineLink';
 import Seo from '@/components/Seo';
 
-import Vercel from '~/svg/Vercel.svg';
+// import Vercel from '~/svg/Vercel.svg';
 
 // !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
 // Before you begin editing, follow all comments with `STARTERCONF`,
@@ -19,7 +20,13 @@ export default function HomePage() {
       <main>
         <section className='bg-white'>
           <div className='layout flex min-h-screen flex-col items-center justify-center text-center'>
-            <Vercel className='text-5xl' />
+            {/* <Vercel className='text-5xl' /> */}
+            <Image
+              src='/favicon/android-chrome-192x192.png'
+              alt='Site Logo'
+              width={200}
+              height={200}
+            />
             <h1 className='mt-4'>Welcome to innodigi.net</h1>
             <p className='mt-2 text-sm text-gray-800'>Coming soon!</p>
 
